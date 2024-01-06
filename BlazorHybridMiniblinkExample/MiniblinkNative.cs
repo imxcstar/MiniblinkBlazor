4f8c1dede04e2b672cd8a926ad9d506afbaba7df
xcssa <imxcssa@qq.com>
2024-01-06 18:15:29 +0800

    添加项目文件。

---
+++ BlazorHybridMiniblinkExample/MiniblinkNative.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorHybridMiniblinkExample
+{
+    public static partial class MiniblinkNative
+    {
+        private const string LIBRARY_NAME = "miniblink_4949_x32";
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate bool WkeLoadUrlBeginCallback(IntPtr webView, IntPtr param, [MarshalAs(UnmanagedType.LPUTF8Str)] string url, IntPtr job);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void wkeConsoleCallback(IntPtr webView, IntPtr param, wkeConsoleLevel level, IntPtr message, IntPtr sourceName, uint sourceLine, IntPtr stackTrace);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate long wkeJsNativeFunction(IntPtr jsExecState, IntPtr param);
+
+        [DllImport(LIBRARY_NAME, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void wkeInitializeEx(IntPtr settings);
+
+        [DllImport(LIBRARY_NAME, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void wkeInitialize();
+
+        [DllImport(LIBRARY_NAME, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void wkeInit();
+
+        [DllImport(LIBRARY_NAME, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr wkeCreateWebWindow(wkeWindowType type, IntPtr parent, int x, int y, int width, int height);
+
+        [DllImport(LIBRARY_NAME, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void wkeShowWindow(IntPtr webWindow, bool show);
+
+        [DllImport(LIBRARY_NAME, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void wkeLoadW(IntPtr webWindow, [MarshalAs(UnmanagedType.LPWStr)] string url);
+
+        [DllImport(LIBRARY_NAME, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void wkeOnLoadUrlBegin(IntPtr webWindow, WkeLoadUrlBeginCallback callback, IntPtr callbackParam);
+
+        [DllImport(LIBRARY_NAME, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr wkeNetGetMIMEType(IntPtr job, IntPtr mime);
+
+        [DllImport(LIBRARY_NAME, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern void wkeNetSetMIMEType(IntPtr job, string type);
+
+        [DllImport(LIBRARY_NAME, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern void wkeNetSetData(IntPtr job, [MarshalAs(UnmanagedType.LPArray)] byte[] buf, int len);
+
+        [DllImport(LIBRARY_NAME, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void wkeOnConsole(IntPtr webView, wkeConsoleCallback callback, IntPtr callbackParam);
+
+        [DllImport(LIBRARY_NAME, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr wkeGetStringW(IntPtr wkeString);
+
+        [DllImport(LIBRARY_NAME, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
+        public static extern Int64 wkeRunJSW(IntPtr webView, string script);
+
+        [DllImport(LIBRARY_NAME, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern void wkeJsBindFunction(string name, wkeJsNativeFunction fn, IntPtr param, uint argCount);
+
+        [DllImport(LIBRARY_NAME, CallingConvention = CallingConvention.Cdecl)]
+        public static extern long jsArg(IntPtr es, int argIdx);
+
+        [DllImport(LIBRARY_NAME, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr jsToTempStringW(IntPtr es, long v);
+
+        [DllImport(LIBRARY_NAME, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void wkeResize(IntPtr webView, int w, int h);
+
+        /// <summary>
+        /// 控制台消息等级
+        /// </summary>
+        public enum wkeConsoleLevel
+        {
+            Debug = 4,
+            Log = 1,
+            Info = 5,
+            Warning = 2,
+            Error = 3,
+            RevokedError = 6,
+        }
+
+        public enum wkeWindowType
+        {
+            WKE_WINDOW_TYPE_POPUP,
+            WKE_WINDOW_TYPE_TRANSPARENT,
+            WKE_WINDOW_TYPE_CONTROL
+        }
+    }
+}
---
+++ BlazorHybridMiniblinkExample/Program.cs
@@ -0,0 +1,204 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Runtime.InteropServices;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.FileProviders;
+using System.Reflection;
+using Microsoft.AspNetCore.Components;
+using System.Diagnostics;
+using System.Text;
+using Serilog;
+using Serilog.Events;
+using static BlazorHybridMiniblinkExample.MiniblinkNative;
+using static BlazorHybridMiniblinkExample.Win32Api;
+
+namespace BlazorHybridMiniblinkExample;
+
+class Program
+{
+    static IntPtr hWnd;
+
+    static MiniblinkWebViewManager? m = null;
+
+    static Uri BaseUri = new Uri("https://localhost/");
+
+    static bool isStart = false;
+
+    public static ILogger logger;
+
+    static void Main()
+    {
+        Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .MinimumLevel.Override("BlazorHybridMiniblinkExample", LogEventLevel.Debug)
+                .Enrich.FromLogContext()
+                .WriteTo.Debug()
+                .CreateLogger();
+        logger = Log.ForContext<Program>();
+
+        IntPtr hInstance = Marshal.GetHINSTANCE(typeof(Program).Module);
+        IntPtr hIcon = LoadIcon(hInstance, (IntPtr)32512); // IDI_APPLICATION
+
+        WNDCLASSEX wcex = new WNDCLASSEX();
+        wcex.cbSize = Marshal.SizeOf<WNDCLASSEX>();
+        wcex.style = 0;
+        wcex.lpfnWndProc = WndProcCallback;
+        wcex.cbClsExtra = 0;
+        wcex.cbWndExtra = 0;
+        wcex.hInstance = Marshal.GetHINSTANCE(typeof(Program).Module);
+        wcex.hIcon = hIcon;
+        wcex.hCursor = IntPtr.Zero;
+        wcex.hbrBackground = IntPtr.Zero;
+        wcex.lpszMenuName = null;
+        wcex.lpszClassName = "MyWindowClass";
+        wcex.hIconSm = IntPtr.Zero;
+
+        RegisterClassEx(ref wcex);
+
+        // Create window
+        hWnd = CreateWindowEx(
+            0,
+            "MyWindowClass",
+            "BlazorHybridMiniblinkExample",
+            WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX,
+            100,
+            100,
+            1200,
+            800,
+            IntPtr.Zero,
+            IntPtr.Zero,
+            wcex.hInstance,
+            IntPtr.Zero);
+
+        // Show window
+        ShowWindow(hWnd, SW_SHOWNORMAL);
+
+        MiniblinkNative.wkeInitializeEx(IntPtr.Zero);
+
+        MiniblinkNative.wkeJsBindFunction("MiniblinkPostMessage", static (IntPtr jsExecState, IntPtr param) =>
+        {
+            var message = MiniblinkNative.jsArg(jsExecState, 0);
+            var messageStr = Marshal.PtrToStringAuto(MiniblinkNative.jsToTempStringW(jsExecState, message));
+            Program.logger.Debug($"MiniblinkPostMessage: {messageStr}");
+            m!.MessageReceived(messageStr!);
+            return 0;
+        }, IntPtr.Zero, 1);
+
+        var wh = GetWindowSize(hWnd);
+        var webWindow = MiniblinkNative.wkeCreateWebWindow(MiniblinkNative.wkeWindowType.WKE_WINDOW_TYPE_CONTROL, hWnd, 0, 0, wh.width, wh.height);
+
+        MiniblinkNative.wkeOnConsole(webWindow, static (IntPtr webView, IntPtr param, wkeConsoleLevel level, IntPtr message, IntPtr sourceName, uint sourceLine, IntPtr stackTrace) =>
+        {
+            var messageStr = Marshal.PtrToStringAuto(MiniblinkNative.wkeGetStringW(message));
+            var sourceNameStr = Marshal.PtrToStringAuto(MiniblinkNative.wkeGetStringW(sourceName));
+            Program.logger.Debug($"wkeOnConsole({level})({sourceNameStr})({sourceLine}): {messageStr}");
+        }, IntPtr.Zero);
+
+
+        MiniblinkNative.wkeOnLoadUrlBegin(webWindow, static (IntPtr webView, IntPtr param, string url, IntPtr job) =>
+        {
+            Program.logger.Debug($"wkeOnLoadUrlBegin: {url}");
+            var ruri = new Uri(url);
+            if (ruri.Host != "localhost")
+                return false;
+
+            var allowFallbackOnHostPage = BaseUri.IsBaseOfPage(url);
+            var requestWrapper = new WebResourceRequest
+            {
+                RequestUri = url,
+                AllowFallbackOnHostPage = allowFallbackOnHostPage,
+            };
+
+            var bRet = m!.PlatformWebViewResourceRequested(requestWrapper, out var response);
+
+            if (!bRet || response is null)
+            {
+                Program.logger.Debug($"wkeOnLoadUrlBegin(404): {url}");
+                MiniblinkNative.wkeNetSetMIMEType(job, "text/html");
+                var stringBytes = Encoding.UTF8.GetBytes("404");
+                MiniblinkNative.wkeNetSetData(job, stringBytes, stringBytes.Length);
+                return true;
+            }
+
+            var headerString = response.Headers[QueryStringHelper.ContentTypeKey];
+            Program.logger.Debug($"wkeOnLoadUrlBegin_headerString: {headerString}");
+
+            using var ms = new MemoryStream();
+            response.Content.CopyTo(ms);
+
+            MiniblinkNative.wkeNetSetMIMEType(job, headerString);
+            var requestData = ms.ToArray();
+            MiniblinkNative.wkeNetSetData(job, requestData, requestData.Length);
+
+            Program.logger.Debug($"wkeOnLoadUrlBegin_wkeNetSetData: {requestData.Length}");
+            return true;
+        }, IntPtr.Zero);
+
+        MiniblinkNative.wkeShowWindow(webWindow, true);
+
+
+        var services = new ServiceCollection();
+        services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true));
+
+        services.AddBlazorWebView();
+
+        services.AddSingleton<JSComponentConfigurationStore>();
+
+        services.AddSingleton<TDispatcher>(provider => new TDispatcher());
+
+        services.AddSingleton<IJSComponentConfiguration>(provider => new JsComponentConfigration(provider.GetRequiredService<JSComponentConfigurationStore>()));
+
+        var sb = services.BuildServiceProvider();
+
+        var appRootDir = AppContext.BaseDirectory;
+        var hostPageFullPath = Path.GetFullPath(Path.Combine(appRootDir, "wwwroot/index.html"));
+        var contentRootDirFullPath = Path.GetDirectoryName(hostPageFullPath)!;
+        var hostPageRelativePath = Path.GetRelativePath(contentRootDirFullPath, hostPageFullPath);
+        var b = new BlazorWebViewHandlerProvider();
+        var f = b.CreateFileProvider(typeof(Routes).Assembly, contentRootDirFullPath);
+        m = new MiniblinkWebViewManager(webWindow, sb, sb.GetRequiredService<TDispatcher>(), BaseUri, f, sb.GetRequiredService<JSComponentConfigurationStore>(), hostPageRelativePath);
+        m.AddRootComponentAsync(typeof(Routes), "#app", ParameterView.Empty).Wait();
+
+        Program.logger.Debug("Start...");
+        m.Navigate("/");
+
+        isStart = true;
+
+        MSG msg;
+        while (GetMessage(out msg, IntPtr.Zero, 0, 0))
+        {
+            if (isStart && m != null)
+            {
+                if (m.MessageQueue.TryDequeue(out var script))
+                {
+                    MiniblinkNative.wkeRunJSW(m.WebView, script);
+                }
+            }
+            TranslateMessage(ref msg);
+            DispatchMessage(ref msg);
+        }
+
+        DestroyWindow(hWnd);
+    }
+
+    public static IntPtr WndProcCallback(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam)
+    {
+        switch (msg)
+        {
+            case WM_DESTROY:
+            case WM_CLOSE:
+                // Exit the application when window is closed
+                PostQuitMessage(0);
+                return IntPtr.Zero;
+            case WM_SIZE:
+                if (isStart && m != null)
+                {
+                    var wh = GetWindowSize(hWnd);
+                    MiniblinkNative.wkeResize(m.WebView, wh.width, wh.height);
+                }
+                return DefWindowProc(hWnd, msg, wParam, lParam);
+            default:
+                return DefWindowProc(hWnd, msg, wParam, lParam);
+        }
+    }
+}
